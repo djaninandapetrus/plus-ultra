730a4c2dc4f060c6e96ab49be7a05d9fd9a7a3eb
djaninandapetrus <djaninandapetrus@gmail.com>
2024-06-18 16:34:40 +0200

    Add files via upload
    Petrus completed task4 (adding models in admin)
    i also fixed an error in my models

--- john/admin.py
+++ john/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import user, PetrusProject, PetrusSkill
 
-# Register your models here.
+class userInline(admin.TabularInline):
+    model = user
+    extra = 1      
+@admin.register(user)
+class UserAdmin(admin.ModelAdmin):
+    inlines = [userInline,]
+
+
+class PetrusProjectInline(admin.TabularInline):
+    model = PetrusProject
+    extra = 3
+@admin.register(PetrusProject)
+class PetrusProjectAdmin(admin.ModelAdmin):
+    inlines = [PetrusProjectInline,]
+
+
+class PetrusSkillInline(admin.TabularInline):
+    model = PetrusSkill
+    extra = 3
+@admin.register(PetrusSkill)
+class PetrusSkillAdmin(admin.ModelAdmin):
+    inlines = [PetrusSkillInline,]
+
--- john/models.py
+++ john/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 class user(models.Model):
-    name = models.charfield(max_length=20)
-    greetings_1 = models.charfield(max_length=5)
-    greetings_2 = models.charfield(max_length=5)
+    name = models.CharField(max_length=20)
+    greetings_1 = models.CharField(max_length=5)
+    greetings_2 = models.CharField(max_length=5)
     picture = models.ImageField(upload_to='picture/')
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self): 
         return self.name
     
-class Project(models.Model):
-    user = models.ForeignKey(user, on_delete=models.CASCADE)
+class PetrusProject(models.Model):
+    User = models.ForeignKey(user, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     technologies_used = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title   
-class Skill(models.Model):
-    user = models.ForeignKey(user, on_delete=models.CASCADE)
+class PetrusSkill(models.Model):
+    User = models.ForeignKey(user, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
 
